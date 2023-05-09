3b4a3618f177ca5f94baf409e303bbd70136e5dd
Le Kiet <kietkentan@gmail.com>
2023-05-10 02:01:01 +0700

    add view

--- app/src/main/java/com/khtn/teachproject/activity/MainActivity.kt
+++ app/src/main/java/com/khtn/teachproject/activity/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.khtn.teachproject.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
 import com.khtn.teachproject.R
+import com.khtn.teachproject.fragment.BlankFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment = BlankFragment()
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.layout, fragment)
+        transaction.commit()
+        supportFragmentManager.executePendingTransactions()
     }
 }
